d09db992ee20de0680e7ebc5ff8174dfa4f4bca0
tim-ning <87076278+tim-ning@users.noreply.github.com>
2021-11-06 12:45:18 -0500

    BC-206 View loan payment history

--- src/main/java/com/ssutopia/finacial/loanService/repository/LoanPaymentRepository.java
+++ src/main/java/com/ssutopia/finacial/loanService/repository/LoanPaymentRepository.java
@@ -1,10 +1,22 @@
 package com.ssutopia.finacial.loanService.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.ssutopia.finacial.loanService.dto.LoanPaymentDto;
 import com.ssutopia.finacial.loanService.entity.LoanPayments;
 
 @Repository
 public interface LoanPaymentRepository extends JpaRepository<LoanPayments, Long> {
+
+    @Query(
+            "select new com.ssutopia.finacial.loanService.dto.LoanPaymentDto" +
+                    "( c.id, c.loan.id, c.account_id, c.amount, c.time_stamp, c.status )" +
+                    "from LoanPayments c where c.loan.id = ?1"
+    )
+    List<LoanPaymentDto> findPaymentsByLoanId(Long loanId);
+    
 }
